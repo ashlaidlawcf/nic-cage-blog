9ec6d5c9b7bc148208e7ee0cd90e4e3821210153
Ash Laidlaw and Zach Swanson <ash@>
2017-03-22 11:42:20 -0700

    add addpost form to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll("post");
+  },
+  actions: {
+    addPost(params) {
+      var post = this.store.createRecord("post", params);
+      post.save();
+      this.transitionTo("index");
+    }
   }
 });
