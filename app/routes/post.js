06f8f32c73c54660af561aad847fcac6fff765b4
Ash Laidlaw and Zach Swanson <zach@>
2017-03-22 13:48:18 -0700

    add delete component to post route

---
+++ app/routes/post.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model(params) {
+      return this.store.findRecord('post', params.post_id);
+    },
+    actions: {
+      deletePost(post) {
+        post.destroyRecord();
+        this.transitionTo("index");
+      }
+    }
+});
